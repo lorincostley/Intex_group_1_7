92e44bb911914f3abdc02fc39a099087b0189fc5
lorincostley <120672798+lorincostley@users.noreply.github.com>
2024-04-08 11:52:47 -0600

    Add project files.

---
+++ Intex_group_1_7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intex_group_1_7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
